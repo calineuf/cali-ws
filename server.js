03a62b7d4e928d138a003c189c71b65ded945f4a
Caroline Bouchard <cbouchard@Callies-Big-Book.local>
2018-02-25 17:33:56 -0800

    Doing some initial work for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -21,12 +22,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs', {
-        printTitle: 'Oh No!',
-        ohNoMessage: 'The internet is dead!  Run for your lives!'
-    });
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         printTitle: 'Oh No!',
+//         ohNoMessage: 'The internet is dead!  Run for your lives!'
+//     });
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -58,6 +59,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
